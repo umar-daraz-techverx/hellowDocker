8a9ed5d6e15d0a7704467462fe39fca66aae9ef2
umar-draz-dotnet <bc190200775@vu.edu.pk>
2020-04-25 21:58:17 +0500

    init

---
+++ hellow/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace hellow
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel(option => { option.Listen(IPAddress.Any, 80); });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
